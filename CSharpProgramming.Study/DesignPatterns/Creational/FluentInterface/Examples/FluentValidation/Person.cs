09d2c1423aaac1c60d5ff598a88c39a1df0d1ca7
pushpa.info.14@gmail.com <pushpa.info.14@calcey.com>
2024-04-16 19:50:58 +0530

    moved to seperate class files

---
+++ CSharpProgramming.Study/DesignPatterns/Creational/FluentInterface/Examples/FluentValidation/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace CSharpProgramming.Study.DesignPatterns.Creational.FluentInterface.Examples.FluentValidation;
+
+public class Person
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public int Age { get; set; }
+}
