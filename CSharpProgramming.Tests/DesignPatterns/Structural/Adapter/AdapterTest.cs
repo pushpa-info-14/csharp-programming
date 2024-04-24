f87f5d3dbefa719324f1eea2d5e1272f3c6bdec8
pushpa.info.14@gmail.com <pushpa.info.14@calcey.com>
2024-04-24 21:41:37 +0530

    adapter design pattern

---
+++ CSharpProgramming.Tests/DesignPatterns/Structural/Adapter/AdapterTest.cs
@@ -0,0 +1,26 @@
+using CSharpProgramming.Study.DesignPatterns.Structural.Adapter;
+using CSharpProgramming.Study.DesignPatterns.Structural.Adapter.Examples;
+
+namespace CSharpProgramming.Tests.DesignPatterns.Structural.Adapter
+{
+    public class AdapterTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Console.WriteLine("\n---- ObjectAdapterExample");
+            new ObjectAdapterExample().Test();
+
+            Console.WriteLine("\n---- ClassAdapterExample");
+            new ClassAdapterExample().Test();
+
+            Console.WriteLine("\n---- LanguageTranslatorProgram");
+            new LanguageTranslatorProgram().Test();
+        }
+    }
+}
