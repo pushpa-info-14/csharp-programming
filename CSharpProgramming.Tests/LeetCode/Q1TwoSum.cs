6f381c2ea5798e7d8e5f1932c06e8174c8f21a73
pushpa-info-14 <pushpa.info.14@gmail.com>
2025-02-13 20:58:22 +0530

    leetcode 1

---
+++ CSharpProgramming.Tests/LeetCode/Q1TwoSum.cs
@@ -0,0 +1,42 @@
+﻿// Ignore Spelling: nums
+
+using CSharpProgramming.Tests.Base;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace CSharpProgramming.Tests.LeetCode;
+
+public class Q1TwoSum(ITestOutputHelper output) : BaseTest(output)
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var res = new int[2];
+
+            Dictionary<int, int> map = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (map.TryGetValue(nums[i], out var index))
+                {
+                    res[0] = index;
+                    res[1] = i;
+                    return res;
+                }
+                map[target - nums[i]] = i;
+            }
+
+            return res;
+        }
+    }
+
+    [Fact]
+    public void Test()
+    {
+        Solution s = new Solution();
+        WriteLine(s.TwoSum([2, 7, 11, 15], 9));
+        WriteLine(s.TwoSum([3, 2, 4], 6));
+        WriteLine(s.TwoSum([3, 3], 6));
+    }
+}
