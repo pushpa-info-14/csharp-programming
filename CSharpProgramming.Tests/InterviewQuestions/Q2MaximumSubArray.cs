88e09d2843eddf78d9051cd0ce3922bfce3636bd
pushpa-info-14 <pushpa.info.14@gmail.com>
2025-02-20 00:02:14 +0530

    more solutions

---
+++ CSharpProgramming.Tests/InterviewQuestions/Q2MaximumSubArray.cs
@@ -0,0 +1,83 @@
+﻿using CSharpProgramming.Tests.Base;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace CSharpProgramming.Tests.InterviewQuestions;
+
+public class Q2MaximumSubArray(ITestOutputHelper output) : BaseTest(output)
+{
+    public class Solution
+    {
+        public int MaxSubArraySum(int[] nums)
+        {
+            int maxSum = -int.MaxValue;
+            int curentSum = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                curentSum += nums[i];
+
+                if (curentSum > maxSum)
+                {
+                    maxSum = curentSum;
+                }
+
+                if (curentSum < 0)
+                {
+                    curentSum = 0;
+                }
+            }
+
+            return maxSum;
+        }
+
+        public int[] MaxSubArray(int[] nums)
+        {
+            int maxSum = -int.MaxValue;
+            int curentSum = 0;
+            int startIndex = -1;
+            int endIndex = -1;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (curentSum == 0) startIndex = i;
+
+                curentSum += nums[i];
+
+                if (curentSum > maxSum)
+                {
+                    maxSum = curentSum;
+                    endIndex = i;
+                }
+
+                if (curentSum < 0)
+                {
+                    curentSum = 0;
+                }
+            }
+
+            var res = new int[endIndex - startIndex + 1];
+            for (int i = startIndex; i < endIndex + 1; i++)
+            {
+                res[i - startIndex] = nums[i];
+            }
+            return res;
+        }
+    }
+
+    [Fact]
+    public void Test()
+    {
+        var s = new Solution();
+        Print(s.MaxSubArraySum([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
+        Print(s.MaxSubArraySum([1]));
+        Print(s.MaxSubArraySum([5, 4, -1, 7, 8]));
+        Print(s.MaxSubArraySum([-1]));
+        Print(s.MaxSubArraySum([3, 2, -6, 8, -3, 5]));
+
+        Print(s.MaxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
+        Print(s.MaxSubArray([1]));
+        Print(s.MaxSubArray([5, 4, -1, 7, 8]));
+        Print(s.MaxSubArray([-1]));
+        Print(s.MaxSubArray([3, 2, -6, 8, -3, 5]));
+    }
+}
---
+++ CSharpProgramming.Tests/LeetCode/Q0000-0500/Q76MinimumWindowSubstring.cs
@@ -0,0 +1,71 @@
+﻿using CSharpProgramming.Tests.Base;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace CSharpProgramming.Tests.LeetCode.Q0000_0500;
+
+public class Q76MinimumWindowSubstring(ITestOutputHelper output) : BaseTest(output)
+{
+    public class Solution
+    {
+        public string MinWindow(string s, string t)
+        {
+            var map = new int[256];
+            int minLength = int.MaxValue;
+            int startIndex = -1;
+            int l = 0;
+            int r = 0;
+            int n = s.Length;
+            int m = t.Length;
+            int count = 0;
+
+            foreach (var c in t)
+            {
+                map[(int)c] += 1;
+            }
+
+            while (r < n)
+            {
+                if (map[(int)s[r]] > 0)
+                {
+                    count++;
+                }
+                map[(int)s[r]] -= 1;
+
+                while (count == m)
+                {
+                    if (r - l + 1 < minLength)
+                    {
+                        minLength = r - l + 1;
+                        startIndex = l;
+                    }
+
+                    map[(int)s[l]] += 1;
+                    if (map[(int)s[l]] > 0)
+                    {
+                        count--;
+                    }
+
+                    l++;
+                }
+                r++;
+            }
+
+            var res = "";
+            if (startIndex != -1)
+            {
+                res = s.Substring(startIndex, minLength);
+            }
+            return res;
+        }
+    }
+
+    [Fact]
+    public void Test()
+    {
+        var s = new Solution();
+        WriteLine(s.MinWindow("ADOBECODEBANC", "ABC"));
+        WriteLine(s.MinWindow("a", "a"));
+        WriteLine(s.MinWindow("a", "aa"));
+    }
+}
